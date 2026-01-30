ea7779a6220a7cb21b14f0591d0239b5ccfba4aa
Ali Ahmed ElKaramany <aliahmed999416@gmail.com>
2026-01-30 23:10:01 +0200

    feat(admin): Add admin acces filter

---
+++ src/main/java/com/app/filters/AdminFilter.java
@@ -0,0 +1,38 @@
+package com.app.filters;
+
+import com.app.model.Role;
+import com.app.model.User;
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+@WebFilter(urlPatterns = {"/admin/*" ,"/jsp/admin/*"})
+public class AdminFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+
+        HttpSession session = httpRequest.getSession(false);
+
+        User user = (session != null) ? (User) session.getAttribute("loggedUser") : null;
+
+        if(user == null)
+        {
+            httpResponse.sendRedirect(httpRequest.getContextPath() + "/jsp/login.jsp");
+            return;
+        }
+
+        if(user.getRole() != (Role.ADMIN))
+        {
+            httpResponse.sendRedirect(httpRequest.getContextPath() + "/jsp/dashboard.jsp");
+            return;
+        }
+
+        chain.doFilter(request, response);
+    }
+}
